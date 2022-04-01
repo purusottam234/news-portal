aa8baa695b1a05118cea9b83b349e8f573d1a665
Purusottam <purusottamadhikari234@gmail.com>
2022-04-01 09:33:30 +0545

    project ready

--- news/urls.py
+++ news/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('login/',views.signin,name='signin'),
     path('signup/',views.signup,name ='signup'),
     path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
     # path('',views.index,name="index"),
     path('registration/',views.registration,name='registration'),
     # path('login/',views.login,name='login'),
--- news/views.py
+++ news/views.py
@@ -171,6 +171,9 @@ def cards(request):
 def about(request):
     return render(request,"about.html")
 
+def contact(request):
+    return render(request,"contact.html")
+
 
 
  
